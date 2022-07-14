107002aaec367e8234049e8c4ddb8d65503f8190
brokyz <brokyz@foxmail.com>
2022-07-14 16:25:43 +0800

    feat

--- recommendJob.py
+++ recommendJob.py
@@ -6,7 +6,7 @@ data = pd.read_csv("./finalData/finalData.csv")
 def main():
     print("recommendJob.py")
     # backTenJobs("vue")
-    # backTenJobs()
+    backTenJobs()
 
 
 def backTenJobs(*skill):
@@ -20,6 +20,19 @@ def backTenJobs(*skill):
 
     recommend_data = recommendTagData.iloc[np.random.choice(
         range(recommendTagData.shape[0]), 10)].reset_index(drop=True)
+    print(recommend_data)
+
+def backVip(*skill,num):
+    if(not skill):
+        skill = []
+        skill.append(input("请输入您掌握的技能："))
+    tag = rec.recommendTag(skill)
+    print(tag)
+    print(rec.recommend(skill))
+    recommendTagData = data[data["tag"] == tag].reset_index()
+
+    recommend_data = recommendTagData.iloc[np.random.choice(
+        range(recommendTagData.shape[0]), num)].reset_index(drop=True)
     print(recommend_data)
 
 
